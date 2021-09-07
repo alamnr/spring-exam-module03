524d32199b33d7c550f7140dcf4e26093fa5556e
alamnr <alamnr@gmail.com>
2021-09-07 09:30:03 +0600

    What is the difference between checked and unchecked exceptions? Why does Spring prefer unchecked exceptions? What is the data access exception hierarchy?

--- module03-question01/src/main/java/com/spring/professional/exam/tutorial/module03/question01/unchecked/Runner.java
+++ module03-question01/src/main/java/com/spring/professional/exam/tutorial/module03/question01/unchecked/Runner.java
@@ -3,7 +3,7 @@ package com.spring.professional.exam.tutorial.module03.question01.unchecked;
 import com.spring.professional.exam.tutorial.module03.question01.unchecked.dao.PersonDao;
 import com.spring.professional.exam.tutorial.module03.question01.unchecked.ds.Person;
 
-public class Runner {
+public class Runner { 
 	public static void main(String[] args) {
 		PersonDao personDao = new PersonDao();
 		Person person = new Person();
